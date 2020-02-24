290c884f576327ca8d8456b806392c66057597a1
Adam Dai <adamdai97@gmail.com>
2020-02-23 23:08:19 -0800

    ADMM deblur with FT method

---
+++ ADMM_deblur.m
@@ -0,0 +1,60 @@
+clear, close all
+img_path = 'stock_photos/stock_01.jpg';
+I = rgb2gray(im2double(imread(img_path)));
+
+I = I(1:300, 1:500);
+
+% blur kernel
+c_size = 25;
+c = zeros(c_size);
+c(ceil(c_size/2),:) = 3/c_size;
+
+% functions converting a point spread function (convolution kernel) to the
+% corresponding optical transfer function (Fouier multiplier)
+p2o = @(x) psf2otf(x, size(I));
+
+% precompute OTFs
+cFT     = p2o(c);
+cTFT    = conj(p2o(c));
+
+% blur image with kernel
+Ib = ifft2(fft2(I).*cFT);
+
+subplot(1,3,1)
+imshow(I)
+subplot(1,3,2)
+imshow(Ib)
+
+% add noise to blurred image
+%b = Ib + sigma.*randn(size(I));
+b = Ib;
+
+% ADMM
+[H,W] = size(I);
+iterations = 100;
+lamda = 0.001;
+rho = 10;
+kappa = lamda/rho;
+
+x = zeros(H, W);
+z = zeros(H, W);
+u = zeros(H, W);
+
+bFT = fft2(b);
+denom = cTFT.*cFT + rho;
+
+for iters = 1:iterations
+    % x update
+    v = z - u;
+    vFT = fft2(v);
+    num = cTFT.*bFT + rho*vFT;
+    x = real(ifft2(num./denom));
+    % z update
+    w = x + u;
+    z = max(w-kappa,0) - max(-w-kappa,0);
+    % u update
+    u = u + x - z;
+end
+
+subplot(1,3,3)
+imshow(x)
