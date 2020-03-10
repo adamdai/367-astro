0bc585a2dfaf5303bd7cdc9a3bd9304a30e7c667
Adam Dai <adamdai97@gmail.com>
2020-03-09 22:18:46 -0700

    center of rotation detection

---
+++ algcircfit.m
@@ -0,0 +1,12 @@
+function [xc,yc,R,u] = algcircfit(x,y)
+%CIRCFIT  Fits a circle in x,y plane
+%
+% minimum right singular value 
+
+n=length(x);  
+B = [x.^2+y.^2, x, y, ones(n,1)];
+[U,S,V] = svd(B);
+u = V(:,end);
+xc = -.5*u(2)/u(1);
+yc = -.5*u(3)/u(1);
+R  =  sqrt((u(2)^2+u(3)^2)/(4*u(1)^2)-u(4)/u(1));
