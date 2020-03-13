b1152264e0aa252d88ac8dae6cb6907e54677c26
David Whisler <dwhis428@gmail.com>
2020-03-13 12:39:03 -0700

    clean up cnn code

--- rotation_center_network.py
+++ rotation_center_network.py
@@ -11,8 +11,8 @@ from tensorflow.keras import backend as K
 import cv2
 
 def read_train_data():
-    path = 'neural_network_data_inwindow/'
-    n_images = 100
+    path = 'neural_network_data_inwindow_largescale/'
+    n_images = 1000
     x = []
     y = []
     with open(path+'labels.txt', 'r') as f:
@@ -26,11 +26,10 @@ def read_train_data():
     return np.array(x),np.array(y)
 
 def read_run_data(path):
-    x = cv2.imread(path)
+    x = plt.imread(path)[:,:,1]
     orig_size = x.shape
-
-    x = cv2.cvtColor(x, cv2.COLOR_RGB2GRAY)
     x = cv2.resize(x, (250,250))
+
     x = np.expand_dims(x, axis=2)
     x = np.expand_dims(x, axis=0)
     x = x.astype('float32')
@@ -39,43 +38,17 @@ def read_run_data(path):
 
     return x, scale
 
-def build_small_model():
-    model = Sequential()
-    model.add(Flatten(input_shape=(250,250,1)))
-    model.add(Dense(2))
-
-    model.compile(loss=keras.losses.mean_squared_error,
-              optimizer=keras.optimizers.Adam(),
-              metrics=[])
-
-    model.summary()
-
-    return model
-
 def build_model2():
     model = Sequential()
-    model.add(Conv2D(48, kernel_size=(11, 11),
+    model.add(Conv2D(64, kernel_size=(25, 25),
                  activation='relu',
                  input_shape=(250,250,1)))
-    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(8, 8), strides=(8, 8)))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(128, (5, 5), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(192, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(192, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(3, 3)))
     model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.5))
-    model.add(Dense(1024, activation='relu'))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.5))
+    model.add(Dense(128, activation='relu'))
     model.add(Dense(2))
-
     model.compile(loss=keras.losses.mean_squared_error,
               optimizer=keras.optimizers.Adam(),
               metrics=[])
@@ -92,11 +65,15 @@ def build_model():
     model.add(MaxPooling2D(pool_size=(8, 8), strides=(8, 8)))
     model.add(Conv2D(64, (5, 5), activation='relu'))
     model.add(MaxPooling2D(pool_size=(4, 4), strides=(4, 4)))
-    # model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
-    # model.add(Dropout(0.5))
     model.add(Dense(2))
+
+    model.compile(loss=keras.losses.mean_squared_error,
+              optimizer=keras.optimizers.Adam(),
+              metrics=[])
+
+    model.summary()
 
     model.compile(loss=keras.losses.mean_squared_error,
               optimizer=keras.optimizers.Adam(),
@@ -119,40 +96,41 @@ def train_network():
     y_test = y_test.astype('float32')
 
 
-    model = build_small_model()
+    model = build_model()
 
-    epochs = 30
+    epochs = 50
     batch_size = 5
 
-    model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
+    # model.fit(x_train, y_train,
+    #       batch_size=batch_size,
+    #       epochs=epochs,
+    #       verbose=1,
+    #       validation_data=(x_test, y_test))
 
-    model.save_weights('rotation_center_net_weights_constbrightness')
+    # model.save_weights('rotation_center_net_weights_constbrightness')
+    model.load_weights('rotation_center_net_weights_largescale')
 
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Test loss:', score)
 
-    predictions = model.predict(x_test)
-    plt.ion()
-    fig,ax = plt.subplots(1)
-    plt.show()
-    for i in range(predictions.shape[0]):
-        ax.imshow(np.squeeze(x_test[i]), cmap='gray')
-        circ = Circle((predictions[i,0],predictions[i,1]), radius=50, fill=False, color='red')
-        ax.add_patch(circ)
-        print('Prediction: ', predictions[i,:])
-        print('Actual: ', y_test[i,:])
-        plt.draw()
-        input("Press [enter] to continue.")
-        circ.remove()
+    # predictions = model.predict(x_test)
+    # plt.ion()
+    # fig,ax = plt.subplots(1)
+    # plt.show()
+    # for i in range(predictions.shape[0]):
+    #     ax.imshow(np.squeeze(x_test[i]), cmap='gray')
+    #     circ = Circle((predictions[i,0],predictions[i,1]), radius=50, fill=False, color='red')
+    #     ax.add_patch(circ)
+    #     print('Prediction: ', predictions[i,:])
+    #     print('Actual: ', y_test[i,:])
+    #     plt.draw()
+    #     input("Press [enter] to continue.")
+    #     circ.remove()
 
 
 def run_network(path):
     model = build_model()
-    model.load_weights('rotation_center_net_weights_largescale')
+    model.load_weights('rotation_center_net_weights_constbrightness')
     I = plt.imread(path)
     x, scale = read_run_data(path)
     prediction = model.predict(x)
@@ -165,7 +143,5 @@ def run_network(path):
 
 
 if __name__ == '__main__':
-    run_network('test_circle_detect_images/real_blur3.jpg')
-    # test_path = 'stock_photos/star_trail_test.jpg'
-    # run_network(test_path)
-    # train_network()
+    train_network()
+    # run_network('test_circle_detect_images/sim_blur3.png')
